f513479810217439ccbf5253b3f5a83182f63348
Sara Abouebeid <saraabo@chalmers.se>
2026-06-05 08:48:15 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import React, { useEffect, useMemo, useRef } from 'react'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import L from 'leaflet'
 import { useDashboard } from '../../context/DashboardContext'
+import { aggregateOrigins } from '../../data/visitDataLoader'
 import MapLegend          from './MapLegend'
 import HeatmapLayer       from './HeatmapLayer'
 import OriginLayer        from './OriginLayer'
@@ -19,6 +20,43 @@ L.Icon.Default.mergeOptions({
 // Simrishamn town centre
 const TOWN_CENTER = [55.5567, 14.3523]
 const TOWN_ZOOM   = 13
+
+function OriginAutoFit() {
+  const map = useMap()
+  const { activeLayers, filteredVisits } = useDashboard()
+  const prevOriginsActive = useRef(false)
+
+  const origins = useMemo(() => aggregateOrigins(filteredVisits), [filteredVisits])
+
+  useEffect(() => {
+    const justEnabled = activeLayers.origins && !prevOriginsActive.current
+    const justDisabled = !activeLayers.origins && prevOriginsActive.current
+    prevOriginsActive.current = activeLayers.origins
+
+    if (justEnabled && origins.length) {
+      const points = [TOWN_CENTER, ...origins.map(origin => [origin.lat, origin.lon])]
+      const bounds = L.latLngBounds(points)
+
+      map.flyToBounds(bounds, {
+        padding: [40, 40],
+        maxZoom: 6,
+        duration: 1.1,
+      })
+      return
+    }
+
+    if (justDisabled) {
+      map.flyTo(TOWN_CENTER, TOWN_ZOOM, {
+        animate: true,
+        duration: 1.1,
+        easeLinearity: 0.25,
+      })
+    }
+  }, [activeLayers.origins, map, origins])
+
+  return null
+}
+
 export default function MapView() {
   const { activeLayers } = useDashboard()
 
@@ -39,6 +77,8 @@ export default function MapView() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           maxZoom={19}
         />
+
+        <OriginAutoFit />
 
         {activeLayers.heatmap     && <HeatmapLayer />}
         {activeLayers.origins     && <OriginLayer />}
